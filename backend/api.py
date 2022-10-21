bca257944c45d050003144e3dff3bf9c9f3dc1f8
hiraos <huisipingwei@gmail.com>
2022-10-22 00:40:35 +0900

    mysqlclientのサンプル追加

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,5 @@
 from flask import *
+import MySQLdb
 
 app = Flask(__name__)
 
@@ -13,3 +14,39 @@ def get_comic():
             {"title":"my-hero-academia","author":"Kohei Horikoshi"},
             {"title":"kisei-ju","author":"Hitoshi Iwaaki"}]
     return jsonify(json)
+
+# DB操作サンプル
+# 実行前に sample.sql を使用してcityテーブルを作成しておく
+# mysqlclientを使うパターン
+@app.route("/api/sample-msc",methods=['GET'])
+def db_sample_msc():
+    """ 接続サンプル """
+ 
+    # 接続する 
+    con = MySQLdb.connect(
+            user='sysjam',
+            passwd='sysjam',
+            host='db-mysql',
+            db='sysjam',
+            charset="utf8")
+ 
+    # カーソルを取得する
+    cur= con.cursor()
+     
+    # クエリを実行する
+    sql = "select * from city"
+    cur.execute(sql)
+ 
+    # 実行結果をすべて取得する
+    rows = cur.fetchall()
+    cur.close()
+    con.close()
+    return jsonify(rows)
+    # http://localhost:5000/api/dbsampleで結果表示
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=5000
+    )
